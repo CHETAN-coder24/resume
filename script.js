d0884d946b1f013b142fdb0d78b9be8dbb143b9b
CHETAN-coder24 <chetanshivaprakash@gmail.com>
2024-12-12 12:10:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+$(document).ready(function () {
+    $('nav ul li a').click(function () {
+        $('nav ul li a').removeClass('active');
+        $(this).addClass('active');
+    });
+});
+
